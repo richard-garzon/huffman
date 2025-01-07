4015ef13aca0ab5f07c1b6a888ae2acca2c0473f
richard garzon <garzon.r@gmail.com>
2025-01-06 23:45:26 -0500

    separate out file opening logic to unit test counting logic against test strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+use clap::Parser;
+use std::collections::HashMap;
+
+#[derive(Parser)]
+#[command(name = "huff")]
+#[command(about = "huffman encoder/decoder", long_about = None)]
+
+struct Cli {
+    file_name: Option<String>,
+}
+
+fn count_chars(file: File) -> HashMap<char, usize> {
+    let mut counter: HashMap<char, usize> = HashMap::new();
+
+    let mut reader = BufReader::new(file);
+    let mut buffer = [0; 1024];
+
+    while let Ok(bytes_read) = reader.read(&mut buffer) {
+        if bytes_read == 0 {
+            break;
+        }
+
+        let chunk = &buffer[..bytes_read];
+        for &byte in chunk {
+            if let Some(ch) = char::from_u32(byte as u32) {
+                counter.entry(ch).and_modify(|count| *count += 1).or_insert(1);
+            }
+        }
+    }
+
+    return counter;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let file = match cli.file_name {
+        Some(file_name) => {
+            File::open(&file_name)
+                .unwrap_or_else(|_| panic!("Failed while opening file {}", &file_name))
+        }
+        None => {
+            panic!("Must pass valid file path to huff")
+        }
+    };
+
+    let counter = count_chars(file);
 }
