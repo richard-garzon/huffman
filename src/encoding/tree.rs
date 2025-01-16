da8b64bc3ee3b91e8bd7f2abf9eb37216378390d
richard garzon <garzon.r@gmail.com>
2025-01-15 21:24:47 -0500

    Setup HuffNode for min-heap use, with some tests

--- src/encoding/tree.rs
+++ src/encoding/tree.rs
@@ -1,7 +1,9 @@
 use std::cell::RefCell;
+use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::rc::Rc;
 
+#[derive(Debug)]
 pub struct HuffNode {
     character: Option<char>,
     weight: u32,
@@ -29,6 +31,26 @@ impl HuffNode {
     }
 }
 
+impl PartialEq for HuffNode {
+    fn eq(&self, other: &Self) -> bool {
+        self.weight == other.weight
+    }
+}
+
+impl Eq for HuffNode {}
+
+impl PartialOrd for HuffNode {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for HuffNode {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.weight.cmp(&self.weight)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -41,5 +63,28 @@ mod tests {
 
         assert_eq!(new_node.character.unwrap(), test_char);
         assert_eq!(new_node.weight, test_weight);
+    }
+
+    #[test]
+    fn test_huffnode_ordering() {
+        let heavy_node = HuffNode::new('a', 100);
+        let light_node = HuffNode::new('b', 50);
+
+        // Reversed so that BinaryHeap is a min-heap
+        assert!(light_node > heavy_node);
+    }
+
+    #[test]
+    fn test_min_heap() {
+        let mut min_heap = BinaryHeap::new();
+        let heavy_node = HuffNode::new('a', 100);
+        let light_node = HuffNode::new('b', 50);
+
+        min_heap.push(&heavy_node);
+        min_heap.push(&light_node);
+
+        assert_eq!(min_heap.len(), 2);
+        assert_eq!(min_heap.pop().unwrap(), Some(&light_node).unwrap());
+        assert_eq!(min_heap.pop().unwrap(), Some(&heavy_node).unwrap());
     }
 }
