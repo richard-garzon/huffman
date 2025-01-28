e5d85dc1fedbbedb589665491bb74c31a1fa882b
richard garzon <garzon.r@gmail.com>
2025-01-27 19:35:11 -0500

    implement some code to work with bits and write a test

---
+++ src/encoding/bitstuff.rs
@@ -0,0 +1,62 @@
+use std::io::{self, Write};
+
+struct BitWriter {
+    buf: Vec<u8>,
+    current_byte: u8,
+    bit_position: u8,
+}
+
+impl BitWriter {
+    fn new() -> Self {
+        BitWriter {
+            buf: Vec::new(),
+            current_byte: 0,
+            bit_position: 0,
+        }
+    }
+
+    fn write_bit(&mut self, bit: u8) {
+        if bit > 1 {
+            panic!("Invalid bit val");
+        }
+
+        self.current_byte = (self.current_byte << 1) | bit;
+        self.bit_position += 1;
+
+        if self.bit_position == 8 {
+            self.buf.push(self.current_byte);
+            self.current_byte = 0;
+            self.bit_position = 0;
+        }
+    }
+
+    fn write_bits(&mut self, bits: u64, num_bits: u8) {
+        for i in (0..num_bits).rev() {
+            let bit = ((bits >> i) & 1) as u8;
+            self.write_bit(bit);
+        }
+    }
+
+    fn flush(&mut self) {
+        if self.bit_position > 0 {
+            self.current_byte <<= 8 - self.bit_position;
+            self.buf.push(self.current_byte);
+            self.current_byte = 0;
+            self.bit_position = 0;
+        }
+    }
+
+    fn get_vec(mut self) -> io::Result<Vec<u8>> {
+        self.flush();
+        Ok(self.buf)
+    }
+}
+
+#[test]
+fn test_write_a_bit() {
+    let mut bw = BitWriter::new();
+
+    bw.write_bit(1);
+
+    assert_eq!(bw.get_vec().unwrap(), vec![128]);
+}
--- src/encoding/mod.rs
+++ src/encoding/mod.rs
@@ -1,3 +1,4 @@
+pub mod bitstuff;
 pub mod encoding;
 pub mod frequency;
 pub mod tree;
