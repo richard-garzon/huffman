771638b4e6db8f27264c18d1e37fc81e315e0044
richard garzon <garzon.r@gmail.com>
2025-03-09 00:17:51 -0500

    implement bitreader/iter

--- src/encoding/decoding.rs
+++ src/encoding/decoding.rs
@@ -2,6 +2,8 @@ use crate::encoding::frequency::Freq;
 use crate::encoding::tree::generate_tree;
 use std::collections::HashMap;
 
+use super::tree::HuffNode;
+
 /// how to start decoding...
 /// we should first read the header from the file and rebuild the huffman tree to decode
 /// and then build the prefix table
@@ -9,10 +11,9 @@ use std::collections::HashMap;
 ///
 /// next we just decode the file and restore the data to its original state
 
-pub fn decode_tree_header_with_size(tree_data: Vec<u8>) -> HashMap<char, u8> {
-    let prefix_table: HashMap<char, u8> = HashMap::new();
-
-    prefix_table
+pub fn decode_tree_header_with_size(tree_data: Vec<u8>) -> Option<Box<HuffNode>> {
+    // before implementing this, implement a bit iterator that makes it easy to
+    // move through these vecs of u8 without have to handle indices or nested loops
 }
 
 #[cfg(test)]
@@ -20,8 +21,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_decode_tree_header_size() {
-        let input: Vec<u8> = vec![0b00000000, 0b00000011];
+    fn test_decode_tree_header() {
+        // Assume that we've read the 4 size bytes already and then returned
+        // the encoded tree in a vec
+        let input: Vec<u8> = vec![
+            0b10000000, 0b00000000, 0b00000000, 0b00110000,
+            0b10000000, // This is the encoded tree
+        ];
         let expected = "aaa";
         let mut freq = Freq::new();
         freq.update(expected.as_bytes());
